047253065e094fdb5a3c20b902e7a6f59aeafa5b
jinvnce <johnvincentbugarin@gmail.com>
2024-09-17 19:56:02 +0800

    Add files via upload

---
+++ act2.php
@@ -0,0 +1,133 @@
+<?php
+
+$prof = [
+    'vince' => [
+        'Name' => 'Vincent',
+        'age' => '17',
+        'Skills' => 'Sleeping',
+        'Hobby' => 'Watching anime, reading manga, playing online games, sleeping',
+        'Interest' => 'sleep and coding',
+        'Favorite_Food' => 'Chicken Curry',
+        'Achievement' => 'With Honors last grade 10',
+        'Characteristics' => 'Kind and humble',
+        'pic' => 'https://scontent.fcrk3-3.fna.fbcdn.net/v/t39.30808-6/460273471_1229179408212444_5407651439220501419_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeHtKZace0uVEH5fzwC2WQPiqClL62gW8oioKUvraBbyiLVMuAd5WGnLRcUb17sHtaVYu77unblfIL2NENCoi1MX&_nc_ohc=Rea1r1AGkA4Q7kNvgGn9ZYG&_nc_ht=scontent.fcrk3-3.fna&_nc_gid=AeMuwI8xyfpsfhjO-1a0TDp&oh=00_AYDUqOL6VTMJxblWvt-4EMdp8fOgfLAvz9TXPLnLyg6ulw&oe=66EDD687'
+    ],
+    'vy' => [
+        'Name' => 'Arvy',
+        'age' => '17',
+        'Skills' => ' Playing basketball ',
+        'Hobby' => ' Sleeping, eating',
+        'Interest' => 'Serve another people',
+        'Favorite_Food' => 'Sinigang, kare kare',
+        'Achievement' => 'Pass the grade 11',
+        'Characteristics' => 'Energetic, funny, joker',
+        'pic' => 'https://scontent.fcrk3-3.fna.fbcdn.net/v/t39.30808-6/460162194_1229184818211903_4582977826338475917_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeHTYjv9aFjSrz0Ic_xGd9TbzRHbOwkIIfbNEds7CQgh9hWZdnDHl-qkCr9p-SronclqgWh9pXUab4jIM3YK7kSh&_nc_ohc=o_NFhEx7bncQ7kNvgGFwC2W&_nc_ht=scontent.fcrk3-3.fna&_nc_gid=Aw-_1DcZIVUdOBi9VNEk78c&oh=00_AYAKFN4v3ZU-ME-3yhmhAfiPYmyEF-sEko6q33zvH0TunA&oe=66EE05F2'
+    ],
+    'jims' => [
+        'Name' => 'James',
+        'age' => '17',
+        'Skills' => 'Gaming, Cooking',
+        'Hobby' => ' Playing online games and cats, watching movies',
+        'Interest' => 'Culinary, Music',
+        'Favorite_Food' => 'Adobo, Sisig',
+        'Achievement' => 'With honors last grade 10',
+        'Characteristics' => 'Passionate and dedicated',
+        'pic' => 'https://scontent.fcrk3-4.fna.fbcdn.net/v/t39.30808-6/460143236_1229233011540417_6142815699692662053_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeF-6176FCOJu3b-SLoBrDAFaH-CC0zPN5pof4ILTM83mglSfFiQbQkOTRqSJi68MIMWKaR1RMGSlXKOrrpAJBYx&_nc_ohc=Q9EKLdTVrywQ7kNvgEiqEzF&_nc_ht=scontent.fcrk3-4.fna&_nc_gid=AVR5AhU6YJ7XLbtIQHlkyZD&oh=00_AYDsllWu-OO_SqPEAINviIQXFx3G5H9c1XyjJKlgJ66r0g&oe=66EDFEDE'
+    ],
+    'gab' => [
+        'Name' => 'Gab',
+        'age' => '17',
+        'Skills' => 'Gaming, Drawing',
+        'Hobby' => 'Playing video games',
+        'Interest' => 'Games, Programming, Theories',
+        'Favorite_Food' => 'Pizza and Chicken',
+        'Achievement' => ' Achieved Honors in Benigno “Ninoy” Aquino High School',
+        'Characteristics' => ' Sympathetic, Kind and Gullible 	',
+        'pic' => 'https://scontent.fmnl33-2.fna.fbcdn.net/v/t39.30808-6/361554889_6345743508879655_4814787422899872904_n.jpg?stp=dst-jpg_p526x296&_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeEx7ovnP8_JacGnYMyUlUjTg831dUlRYOiDzfV1SVFg6Pe3GNF3VWtKZBevIaYak6IoAog9B35s7faUL0MrDN1y&_nc_ohc=ew9sOxE8bLYQ7kNvgGaWnpj&_nc_ht=scontent.fmnl33-2.fna&oh=00_AYBef_K2lRY3hPH7jonAASMGIq2UPIMhOEJksHmkpA9yMA&oe=66EC7B65'
+    ],
+    'shaeb' => [
+        'Name' => 'Shaib',
+        'age' => '27',
+        'Skills' => 'Physical prowess',
+        'Hobby' => 'Singing, drawing, gaming, exercising',
+        'Interest' => 'Art, games, working out',
+        'Favorite_Food' => 'Anything that tastes good with protein',
+        'Achievement' => 'Grade 10 with honors 90 😎',
+        'Characteristics' => 'Joyful',
+        'pic' => 'https://scontent.fcrk3-1.fna.fbcdn.net/v/t1.15752-9/459551951_1599888310565748_4047881257852142764_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=9f807c&_nc_eui2=AeHpXA6ad2VXXmVG3iMBWszizf-g6DVFIYTN_6DoNUUhhNOFMRYlI9378_JDry5x682tb-Lz6hzjwG9fsinXrisw&_nc_ohc=JoVczMpOU5IQ7kNvgERDdFn&_nc_ht=scontent.fcrk3-1.fna&_nc_gid=AbXh8Y3QXIXvErEIN7dPCzT&oh=03_Q7cD1QF1uSicO2-RUIZhBBxpwaGxnsT6SJsvhCRWsaBxnbYzKA&oe=670FAAD1'
+    ],
+    'ren' => [
+        'Name' => 'Daren',
+        'age' => '19',
+        'Skills' => 'Cooking',
+        'Hobby' => 'Playing online games, playing basketball',
+        'Interest' => 'Cooking',
+        'Favorite_Food' => 'Tortang talong',
+        'Achievement' => 'When I was 14 yrs old, I was able to help my parents to pay bills due to my work.',
+        'Characteristics' => 'Eager, ambitious',
+        'pic' => 'https://scontent.fcrk3-2.fna.fbcdn.net/v/t39.30808-6/459925031_1229229171540801_950456298847185850_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeE3y1JxM3V-j-0QULU72gzsRsIRVXb1M_RGwhFVdvUz9CgB8dZR41OZFdWRfXEsuuTIfiT_6O-uJovMYbDGz8xA&_nc_ohc=bg_xMNcscCMQ7kNvgFKnIWG&_nc_ht=scontent.fcrk3-2.fna&oh=00_AYBVZRb4YhW7MEIUNaOopAuS-uFiqVzjPbl3O2ldZKmlow&oe=66EDFF4C'
+    ],
+    'ely' => [
+        'Name' => 'Elyron',
+        'age' => 'Its complicated',
+        'Skills' => 'Playing musical instrument',
+        'Hobby' => 'Playing basketball/ rehearsing with my instruments',
+        'Interest' => 'Travel',
+        'Favorite_Food' => 'Sisig',
+        'Achievement' => 'Played at the concert at the park (luneta park)',
+        'Characteristics' => 'Empathetic',
+        'pic' => 'https://scontent.fcrk3-2.fna.fbcdn.net/v/t39.30808-6/459975515_1229196111544107_8972975743573140807_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeEnji0VkEQrod6KnbkwOzHXNcgloFf_iP41yCWgV_-I_gzz3qsbt_J4-4OGTE-kj5ZR-alIElTmRnf48FUVl0xq&_nc_ohc=LVjmev_n-PMQ7kNvgHbJtbj&_nc_ht=scontent.fcrk3-2.fna&_nc_gid=Act9ZWbmAlUv9IXpdJ-Y2Y0&oh=00_AYBB0aOPgCjCr985-nrst_dbTtKboYxI6GIMkQI1QFx9Yg&oe=66EE06F7'
+    ],
+];
+
+$user = isset($_GET['user']) ? htmlspecialchars($_GET['user']) : 'Guest';
+
+$profile = [
+    'Name' => 'Unknown',
+    'age' => 'Unknown',
+    'Skills' => 'Unknown',
+    'Hobby' => 'Unknown',
+    'Interest' => 'Unknown',
+    'Favorite_Food' => 'Unknown',
+    'Achievement' => 'Unknown',
+    'Characteristics' => 'Unknown',
+    'pic' => 'Unknown'
+];
+
+if (isset($prof[$user])) {
+    $profile = $prof[$user];
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About us</title>
+    <link rel="stylesheet" href="act2.css"> 
+</head>
+<body>
+    <div class="box-box"></div>
+    <div class="anothercontain">
+    <div class="rectangle"></div>
+    <div class="label-container">
+            <a href="act2.html">
+                <label>
+                    <img src="https://scontent.fcrk3-3.fna.fbcdn.net/v/t39.30808-6/460273449_1229172671546451_1409307746273933495_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGLV0xb4UXyQPO1UK16gsXZFFh5J4vfF1QUWHkni98XVISxtUg12e6GFgJR3e-nEIslokeB3e-b_JMUWJ8krGf1&_nc_ohc=uesX9mRi6_oQ7kNvgH2aIss&_nc_ht=scontent.fcrk3-3.fna&_nc_gid=AGslOzb02-O7OOhqdbsmdQx&oh=00_AYBl9ckUNDz_0wG3a65Wn72R8394ytXqZxdDKrGpnGqtwg&oe=66EDD0CD" alt="PNG CAN'T DISPLAY" class="label-img">
+                    T.U-BULL
+                </label>
+            </a>
+        </div>
+    <div class="prof-pic">
+    <img src="<?php echo $profile['pic']; ?>" alt="<?php echo $profile['Name']; ?> Picture">
+    <div class="profile-info">    
+        <h1>Hi, I'm <?php echo $profile['Name']; ?>!</h1>
+        <p><strong>Age:</strong> <?php echo $profile['age']; ?></p>
+        <p><strong>Hobby:</strong> <?php echo $profile['Hobby']; ?></p>
+        <p><strong>Interest:</strong> <?php echo $profile['Interest']; ?></p>
+        <p><strong>Favorite Food:</strong> <?php echo $profile['Favorite_Food']; ?></p>
+        <p><strong>Achievement:</strong> <?php echo $profile['Achievement']; ?></p>
+        <p><strong>Characteristics:</strong> <?php echo $profile['Characteristics']; ?></p>
+    </div>
+</body>
+</html>
